706e4224645ccb101e9bcdaf4fd1aad56d82e326
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-04-08 12:08:26 +0200

    Change BookCard from functional to class component

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import TextTruncate from "react-text-truncate";
 
-const BookCard = props => {
-    const { volumeInfo } = props.info;
-    const { title, description } = volumeInfo;
+class BookCard extends Component {
+    getThumbnail = volumeInfo => {
+        return volumeInfo.imageLinks
+            ? volumeInfo.imageLinks.thumbnail
+            : "https://vignette.wikia.nocookie.net/pandorahearts/images/a/ad/Not_available.jpg/revision/latest?cb=20141028171337";
+    };
 
-    const getThumbnail = volumeInfo.imageLinks
-        ? volumeInfo.imageLinks.thumbnail
-        : "https://vignette.wikia.nocookie.net/pandorahearts/images/a/ad/Not_available.jpg/revision/latest?cb=20141028171337";
+    render() {
+        const { volumeInfo } = this.props.info;
+        const { title, description } = volumeInfo;
 
-    return (
-        <div className="card-container">
-            <img src={getThumbnail} alt="book" />
-            <div className="desc">
-                <h2>{title}</h2>
-                <p>
-                    <TextTruncate
-                        line={4}
-                        truncateText="…"
-                        text={description}
-                    />
-                </p>
+        return (
+            <div className="card-container">
+                <img src={this.getThumbnail(volumeInfo)} alt={title} />
+                <div className="desc">
+                    <h2>{title}</h2>
+                    <p>
+                        <TextTruncate
+                            line={4}
+                            truncateText="…"
+                            text={description}
+                        />
+                    </p>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default BookCard;
